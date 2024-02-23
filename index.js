74cc47f48dc8886bec0e6adb62d9861082a961ff
Me <justin_hunter@hotmail.com>
2024-02-22 21:52:14 -0700

    initial setup

---
+++ index.js
@@ -0,0 +1,58 @@
+// import packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const gmd = require('./utils/generateMarkdown.js');
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: "What is your GitHub username? (No @ needed)",
+        name: 'username',
+        default: '',
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'repo',
+        default: '',
+    },
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title',
+        default: 'Project Title',
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        default: 'Project Description',
+    },
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide instructions on how to use your project.",
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['Apache License 2.0', 'GNU GPL 3', 'MIT License', 'BSD 2 Simple', 'BSD 3 New'],
+        name: 'license'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
